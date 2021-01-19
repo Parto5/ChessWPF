c39737970693efafa2d78f0b15cf4e409db22faa
Patryk Piasecki <patryk.piasecki@student.wat.edu.pl>
2021-01-19 22:02:41 +0100

    Stworzony board gridem i zaczęto kodowanie

---
+++ ChessWPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChessWPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //enum dla typu zawartości każdego pola
+        private SquareType[] square;
+
+        private bool whiteTurn;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            square = new SquareType[64];
+
+            for (int i = 0; i < square.Length; i++)
+                square[i] = SquareType.Black_Pawn;
+
+            whiteTurn = true;
+            
+            //próbowałem złapać obraz z przycisków ale te nie są dziećmi 
+            /*
+            Container.Children.Cast<Image>().ToList().ForEach(image =>
+            {
+                image.Source = new BitmapImage(new Uri(@"images\FreeSpace.png"));
+            }); */
+        }
+    }
+}
---
+++ ChessWPF/SquareType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessWPF
+{
+    public enum SquareType
+    {
+        Free,
+        White_Rook,
+        White_Bishop,
+        White_Queen,
+        White_King,
+        White_Knight,
+        White_Pawn,
+        Black_Rook,
+        Black_Bishop,
+        Black_Queen,
+        Black_King,
+        Black_Knight,
+        Black_Pawn
+    }
+}
